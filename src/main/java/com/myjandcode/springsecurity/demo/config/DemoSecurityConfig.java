03873f3e1ab057e6d9cec848c876933283ee927c
JAndres PinillaB <andres_pb07@outlook.com>
2023-02-13 20:21:02 -0500

    se actualiza configuracion jdbc demosecurityconfig

--- src/main/java/com/myjandcode/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/myjandcode/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,28 +1,35 @@
 package com.myjandcode.springsecurity.demo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig {
+	
+	// add a reference to our security data source
+	
+	private DataSource securityDataSource;
+
+	@Autowired
+	public DemoSecurityConfig(DataSource theSecurityDataSource) {
+		securityDataSource = theSecurityDataSource;
+	}
 
 	@Bean
-	public InMemoryUserDetailsManager userDetailsManager() {
-
-		UserDetails john = User.builder().username("jhon").password("{noop}test123").roles("EMPLOYEE").build();
-		UserDetails mary = User.builder().username("mary").password("{noop}test123").roles("MANAGER", "EMPLOYEE").build();
-		UserDetails susan = User.builder().username("susan").password("{noop}test123").roles("ADMIN", "EMPLOYEE").build();
-		UserDetails rut = User.builder().username("rut").password("{noop}test123").roles("ADMIN","EMPLOYEE","MANAGER").build();
-		
-		return new InMemoryUserDetailsManager(john, mary, susan, rut);
+	public UserDetailsManager userDetailsManager() {
+		return new JdbcUserDetailsManager(securityDataSource);
 	}
+	
+	//----------------------------------------------------------------
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
